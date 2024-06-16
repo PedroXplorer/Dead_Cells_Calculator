ed8df201dd244c5c878b5fd46f61e98a54b534a1
PedroXplorer <pdrhnqalvesmartins@gmail.com>
2024-06-16 17:17:51 -0300

    Update

--- dc_calc.py
+++ dc_calc.py
@@ -1,6 +1,33 @@
-dano = float(input("Dano inicial: "))
-dano2 = float(input("Dano secundário: "))
-per_a = float(input("Porcentagem de dano adicional: "))
-dano_s = dano + dano2
-dano_t = dano_s + (dano_s *(per_a/100))
-print(f"Dano total da armas: {dano_t}")
+from tkinter import *
+from tkinter import ttk
+
+#criação janela
+janela = Tk()
+janela.geometry("500x500")
+janela.title("Dead Cells Calculator")
+
+#função calculadora
+# def calculator():
+#     try:
+#         dano = float(base.get())
+#         #dano2 = float(input("Dano secundário: "))
+#         #per_a = float(input("Porcentagem de dano adicional: "))
+#         #dano_t = (dano + dano2) + ((dano + dano2) *(per_a/100))
+#         dano.set()
+#     except ValueError:
+#         pass 
+    
+# #campos de entrada
+# base = IntVar()
+# base_entrada = ttk.Entry(textvariable=base)
+ 
+# dano = StringVar()
+# ttk.Label(textvariable=dano)
+
+
+# ttk.Button(textvariable="calcular", command=calculator)
+
+
+
+
+janela.mainloop()
