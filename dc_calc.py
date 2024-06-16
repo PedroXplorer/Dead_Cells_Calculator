5802a1d99be303bb2f6b5a11404971fba928c66c
Skip0s <61255262+SkipOs@users.noreply.github.com>
2024-06-16 17:11:53 -0300

    Update dc_calc.py

--- dc_calc.py
+++ dc_calc.py
@@ -1,6 +1,52 @@
-dano = float(input("Dano inicial: "))
-dano2 = float(input("Dano secundário: "))
-per_a = float(input("Porcentagem de dano adicional: "))
-dano_s = dano + dano2
-dano_t = dano_s + (dano_s *(per_a/100))
-print(f"Dano total da armas: {dano_t}")
+from tkinter import *
+from tkinter import ttk
+
+
+def calculator(*args):
+    try:
+        dano = float(input("Dano inicial: "))
+        dano2 = float(input("Dano secundário: "))
+        per_a = float(input("Porcentagem de dano adicional: "))
+        dano_s = dano + dano2
+        dano_t = dano_s + (dano_s *(per_a/100))
+        print(f"Dano total da armas: {dano_t}")
+    except ValueError:
+        pass 
+
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+root = Tk()
+root.title("Feet to Meters")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+root.mainloop()
+
